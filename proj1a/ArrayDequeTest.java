cc4d0bda16098d3ef242512269860bea440d47b7
Andrew Zhu <andrew@Andrews-Mac-mini.local>
2020-07-18 17:01:36 -0400

    updated project1a

--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class ArrayDequeTest {
-}
+    public static void main(String[] args) {
+        Random random = new Random();
+        ArrayDeque<Integer> nums = new ArrayDeque<>();
+        nums.addLast(1);
+        nums.addLast(2);
+        nums.addLast(3);
+        nums.addLast(4);
+        nums.addLast(5);
+        nums.addLast(6);
+
+
+
+        nums.printDeque();
+
+    }
+ }
