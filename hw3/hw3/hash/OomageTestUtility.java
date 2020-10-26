c5fdda9c5c9085e4ceef4b1d4dcbc502dd143308
Andrew Zhu <andrew@Andrews-Mac-mini.local>
2020-10-26 16:36:19 -0400

    submission 6

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -5,14 +5,6 @@ import java.util.List;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
-        /* TODO:
-         * Write a utility function that returns true if the given oomages
-         * have hashCodes that would distribute them fairly evenly across
-         * M buckets. To do this, convert each oomage's hashcode in the
-         * same way as in the visualizer, i.e. (& 0x7FFFFFFF) % M.
-         * and ensure that no bucket has fewer than N / 50
-         * Oomages and no bucket has more than N / 2.5 Oomages.
-         */
         int size = oomages.size();
         double min = size / 50;
         double max = size / 2.5;
@@ -21,7 +13,9 @@ public class OomageTestUtility {
             int hc = (o.hashCode() & 0x7FFFFFFF) % M;
             buckets[hc]++;
         }
-        for (int i = 0; i < M; i++) { // can't put if statement in previous for-loop because we want to check the number of Oomage objects in each bucket is within (N / 50, N / 2.5)
+        // can't put if statement in previous for-loop
+        // because we want to check the number of Oomage objects in each bucket is within (N / 50, N / 2.5)
+        for (int i = 0; i < M; i++) {
             if (buckets[i] <= min || buckets[i] >= max) {
                 return false;
             }
--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -14,7 +14,6 @@ public class SimpleOomage implements Oomage {
 
     @Override
     public boolean equals(Object o) {
-        // TODO: Write this method.
         if (o == null) {
             return false;
         }
