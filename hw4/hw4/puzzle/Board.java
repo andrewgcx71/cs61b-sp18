89d8aabe7f9a384be92f48e4a8b34d81dd7bc723
Andrew Zhu <andrew@Mac-mini.local>
2020-11-14 20:43:29 -0500

    seventh try

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -206,23 +206,4 @@ public class Board implements WorldState {
         s.append("\n");
         return s.toString();
     }
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        int N = in.readInt();
-        int[][] tiles = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                tiles[i][j] = in.readInt();
-            }
-        }
-
-        Board test = new Board(tiles);
-
-        Solver solver = new Solver(test);
-        StdOut.println(test.estimatedDistanceToGoal());
-        StdOut.println("Minimum number of moves = " + solver.moves());
-        for (WorldState ws : solver.solution()) {
-            StdOut.println(ws);
-        }
-    }
 }
